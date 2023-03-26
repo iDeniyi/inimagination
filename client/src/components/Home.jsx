f66fa3489bf93921cb9d9e5114a431732eb0a96b
iDeniyi <inioluwa@ualberta.cai>
2023-03-26 01:04:52 -0600

    fixed the launch bbutton on the home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 import { IoRocket } from "react-icons/io5";
 
 const Home = () => {
@@ -25,12 +26,14 @@ const Home = () => {
                         through the inimagination.
                     </p>
                     <div>
-                        <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-red-500">
-                            launch
-                            <span>
-                                <IoRocket />
-                            </span>
-                        </button>
+                        <Link to={"portfolio"} smooth duration={500}>
+                            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-red-500">
+                                launch
+                                <span>
+                                    <IoRocket />
+                                </span>
+                            </button>
+                        </Link>
                     </div>
                 </div>
 
